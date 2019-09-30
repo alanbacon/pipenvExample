8e0d8f3d9dc8c0e6f515fca75eae12c5e0b036ae
Alan Bacon <alan.bacon@bbc.co.uk>
2019-10-01 00:36:20 +0100

    change what program prints

--- pipenvExample/main.py
+++ pipenvExample/main.py
@@ -10,7 +10,12 @@ def listfiles():
 
 
 if __name__ == "__main__":
-	print(sys.version)
-	print(os.getcwd())
+	print('This program will print the version of python running, ' 
+		'the current working directory and the files within that directory'
+	)
+	print('')
+	print('python version: ', sys.version)
+	print('cwd: ', os.getcwd())
+	print('')
 	files = listfiles()
 	print(files)

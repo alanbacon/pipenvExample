e93990f2eaa8a2dd94d7b72881f0b9f8934763e8
Alan Bacon <alan.bacon@bbc.co.uk>
2019-10-01 00:36:58 +0100

    only move to next build step if previous one was sucessful

--- buildAndDeployPythonPackage.sh
+++ buildAndDeployPythonPackage.sh
@@ -1,3 +1,3 @@
-pipenv run python -m pipenv2setup
-pipenv run python setup.py sdist bdist_wheel
+pipenv run python -m pipenv2setup &&
+pipenv run python setup.py sdist bdist_wheel &&
 pipenv run twine upload --username $PYPI_USER --password $PYPI_PASS dist/*
